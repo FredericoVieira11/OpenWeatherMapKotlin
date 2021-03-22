b9c06b93460747cef37d4bb5983b5de2ac7c75bb
fredericovieira <developer.frederico.vieira@gmail.com>
2021-03-22 11:03:55 +0000

    Adjust some design

--- app/src/main/java/com/example/openweathermapkotlin/utils/KotlinExtensions.kt
+++ app/src/main/java/com/example/openweathermapkotlin/utils/KotlinExtensions.kt
@@ -8,7 +8,7 @@ import com.google.android.material.snackbar.Snackbar
 
 fun showAlert(context: Context?, view: View, text: String) {
     Snackbar.make(view, text, Snackbar.LENGTH_SHORT)
-            .setBackgroundTint((ContextCompat.getColor(context!!, R.color.purple_500)))
-            .setTextColor((ContextCompat.getColor(context, R.color.white)))
+            .setBackgroundTint((ContextCompat.getColor(context!!, R.color.white)))
+            .setTextColor((ContextCompat.getColor(context, R.color.black)))
             .show()
 }
